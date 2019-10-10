071ace782ff71c9d49be9e3c52e40da54cb23f8c
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-10-10 16:49:52 -0600

    added dependancies and new branch with file structure

---
+++ web-applications-testing/src/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import './App.css';
+// import Display from './components/Display.js';
+// import Dashboard from './components/Dashboard.js';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
