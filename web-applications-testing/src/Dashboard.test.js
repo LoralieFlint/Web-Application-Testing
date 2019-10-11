0643d9ca75094e49ce21198703d7ba41645176c5
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-10-10 18:43:48 -0600

    added test files and ran simple test to run app now to write more testing functions

---
+++ web-applications-testing/src/Dashboard.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import App from "./App.js"
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+afterEach(rtl.cleanup);
+
+test('render the app', () => {
+    const wrapper = rtl.render(<App/>)
+    wrapper.debug()
+    console.log("it works!")
+})
+
+// describe('display module', () => {
+//     it('can add two numbers', () => {
+           
+//     })
+// })
+
+// toBeVisible()
+// toHaveValue()
+// wrapper.debug(wrapper.getByText('The dog website'))
+// wrapper.debug(wrapper.queryByText('The dog website'))
+// wrapper.debug(wrapper.queryByPlaceholderText('Image count'))
